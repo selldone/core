17db7582948ef20d5c6e1a7479fdf2db7a75af0b
Pajuhaan <pajuhaan@gmail.com>
2023-09-25 17:00:02 +0400

    SDK
    Step 20

--- models/community/community.model.ts
+++ models/community/community.model.ts
@@ -12,10 +12,99 @@
  * Tread carefully, for you're treading on dreams.
  */
 
-export interface Community{
+export interface Community {
+  /** Unique identifier for the community */
+  id: number;
 
+  /** Identifier for the associated shop */
+  shop_id: number;
 
+  /** Identifier for the associated user */
+  user_id: number;
+
+  /** Name of the community */
+  name: string;
+
+  /** Title of the community */
+  title: string;
+
+  /** Description of the community */
+  desc: string;
+
+  /** Rules of the community */
+  rule: string;
+
+  /** Image associated with the community */
+  image: string;
+
+  /**
+   * Stage access level determining who can post a new topic.
+   * {@see CommunityStageLevel}
+   */
+  stage: string;
+
+  /**
+   * Determines who can attach files.
+   * {@see CommunityStageLevel}
+   */
+  attachment: string;
+
+  /** Allowed file types for attachments */
+  mims: string[];
+
+  // ⛔ Admin configuration:
+
+  /** Indicates if links should be marked as "nofollow" */
+  nofollow: boolean;
+
+  /** Indicates if topics can be forked */
+  cross: boolean;
+
+  /** Indicates if the community is enabled or not */
+  enable: boolean;
+
+  // Total values:
+
+  /** Total number of categories in the community */
+  total_categories: number;
+
+  /** Total number of topics in the community */
+  total_topics: number;
+
+  /** Total number of posts in the community */
+  total_posts: number;
+
+  /** Total number of comments in the community */
+  total_comments: number;
+
+  // Selldone administrator actions:
+
+  /** Indicates if penalties are applied */
+  penalty: boolean;
+
+  // Access list: (Use to check fast access)
+
+  /** Indicates if the admins list is accessible */
+  admins_list: boolean;
+
+  /** Indicates if the moderators list is accessible */
+  moderators_list: boolean;
+
+  // Bot:
+
+  /**
+   * Configuration for bots in the community.
+   * Example: Publishing on a Telegram channel.
+   * Note: Telegram channel names are saved without the '@' prefix.
+   */
+  bots: {
+    Telegram?: {
+      enable: boolean;
+      channel: string;
+      errors: any[];
+    };
+
+  };
 }
-export namespace Community {
 
-}
+export namespace Community {}
