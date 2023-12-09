b6130b165a3a22748e70ac5d62b7a91a585e24b2
Pajuhaan <pajuhaan@gmail.com>
2023-12-09 16:53:46 +0400

    Update SDK

--- helper/number/NumberHelper.ts
+++ helper/number/NumberHelper.ts
@@ -17,13 +17,13 @@ export class NumberHelper {
     str = "" + str;
     // convert persian digits [۰۱۲۳۴۵۶۷۸۹]
     let e = "۰".charCodeAt(0);
-    str = str.replace(/[۰-۹]/g, function (t) {
+    str = str.replace(/[۰-۹]/g, function (t: string) {
       return t.charCodeAt(0) - e;
     });
 
     // convert arabic indic digits [٠١٢٣٤٥٦٧٨٩]
     e = "٠".charCodeAt(0);
-    str = str.replace(/[٠-٩]/g, function (t) {
+    str = str.replace(/[٠-٩]/g, function (t: string) {
       return t.charCodeAt(0) - e;
     });
     return str;
