ca4c17d5cd2ddabb47718ccebab99f3ba73510c0
Pajuhaan <pajuhaan@gmail.com>
2023-12-23 19:55:00 +0400

    Update SDK

--- helper/csv/product/ProductsCsvFormat.ts
+++ helper/csv/product/ProductsCsvFormat.ts
@@ -12,9 +12,9 @@
  * Tread carefully, for you're treading on dreams.
  */
 
-import { ProductType } from "../../enums/product/ProductType";
-import { Currency } from "../../enums/payment/Currency";
-import { ProductCondition } from "../../enums/product/ProductCondition";
+import { ProductType } from "../../../enums/product/ProductType";
+import { Currency } from "../../../enums/payment/Currency";
+import { ProductCondition } from "../../../enums/product/ProductCondition";
 
 export const ProductsCsvHeaders = [
   "Product ID",
@@ -152,6 +152,36 @@ export const ProductsCsvStyler = {
       color: "#388E3C",
       dark: true,
     },
+    tooltip: {
+      title: "Unique Product ID On Selldone",
+      message:
+        "This is the unique product ID on Selldone. If you want to update the product, you can enter the product ID here. If you want to create a new product, leave this field blank.",
+    },
+  },
+  "Variant ID": {
+    type: "id", // Show ID badge in cell
+    value_exist: {
+      // Show this message if the value exists!
+      title: "Update",
+      message:
+        "We try to find the variant by the variant ID. If the variant exists, we update it, otherwise we create a new variant.",
+      color: "#1976D2",
+      dark: true,
+    },
+    value_empty: {
+      // Show this message if the value is empty!
+      title: "Create",
+      message:
+        "We try to find the variant by the color,weight,style,... and SKU. If the variant exists, we update it. If not, we create a new variant.",
+      color: "#388E3C",
+      dark: true,
+    },
+
+    tooltip: {
+      title: "Unique Variant ID On Selldone",
+      message:
+        "This is the unique variant ID on Selldone. If you want to update the variant, you can enter the variant ID here. If you want to create a new variant, leave this field blank. When this value be empty Selldone check to find variant based on color,volume and etc and if find variant update it.",
+    },
   },
 
   Title: {
@@ -160,12 +190,20 @@ export const ProductsCsvStyler = {
     ifNullCol: "Title", // Same as product if is null!
     ifNullMessage: "🗘 Optional", // Message if col is null!
     onlyProduct: true,
+    tooltip: {
+      title: "Product Title",
+      message: "It's the name of the product. It should be short and clear.<br>e.g: <code>iPhone 15 Pro Max</code>",
+    },
   },
   "Title En": {
     type: "text",
     ifNullCol: "Title En", // Same as product if is null!
     ifNullMessage: "🗘 Optional", // Message if col is null!
     onlyProduct: true,
+    tooltip: {
+      title: "Technical or Latin Product Name",
+      message: "It's the technical or Latin name of the product. Also it can be the model name of the product.<br>e.g: <code>Apple Iphone 15 Pro Max DR5-XDR</code>",
+    },
   },
 
   Type: {
@@ -178,6 +216,10 @@ export const ProductsCsvStyler = {
       },
     ],
     default: ProductType.PHYSICAL.code, // Auto correction
+    tooltip: {
+      title: "Product Type",
+      message: "Selldone supports all product and services type including Physical, Virtual, File,Service, Subscription.Each type has its own fulfillment method and pricing method.<br>e.g: <code>PHYSICAL</code>",
+    },
   },
   Price: {
     type: "price",
@@ -185,6 +227,10 @@ export const ProductsCsvStyler = {
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
     default: 0, // Auto correction
+    tooltip: {
+      title: "Net Price",
+      message: "This is the price of the product.<br>e.g: <code>100</code><br><b>Selling Price = <span style='color: #80ea69'>Price</span> + Commission - Discount </b>",
+    },
   },
 
   Currency: {
@@ -194,29 +240,50 @@ export const ProductsCsvStyler = {
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
     default: Currency.USD.code, // Auto correction
+    tooltip: {
+      title: "Currency",
+      message: "The price, discount and commission of the product are in this currency.<br>e.g: <code>USD</code>",
+    },
   },
   Commission: {
     type: "price",
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
     default: 0, // Auto correction
+    tooltip: {
+      title: "Commission",
+      message: "There is various use case for commission. This value will be added to price for customer, and can use for dynamic commission to affiliate or reseller in wholesale.<br>e.g: <code>10</code><br><b>Selling Price = Price + <span style='color: #80ea69'>Commission</span> - Discount </b>",
+    },
   },
   Discount: {
     type: "price",
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
     default: 0, // Auto correction
+    tooltip: {
+      title: "Discount",
+      message: "This is the discount of the product.<br>e.g: <code>10</code><br><b>Selling Price = Price + Commission - <span style='color: #80ea69'>Discount</span> </b>",
+    },
+
   },
 
   "Discount Start Date": {
     type: "date",
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
+    tooltip: {
+      title: "Discount Start Date",
+      message: "This is the start date of the discount. Format is YYYY-mm-dd h:m:s <br>e.g: <code>2022-08-10 00:00:00</code>",
+    },
   },
   "Discount End Date": {
     type: "date",
     ifNullCol: "Price", // Same as product if is null!
     ifNullMessage: "♺ Like product", // Message if col is null!
+    tooltip: {
+      title: "Discount End Date",
+      message: "This is the end date of the discount. Format is YYYY-mm-dd h:m:s <br>e.g: <code>2022-08-20 00:00:00</code>",
+    },
   },
 
   Status: {
@@ -226,22 +293,38 @@ export const ProductsCsvStyler = {
     required_variant: true,
     default: "Open", // Autocorrection
     onlyProduct: true,
+    tooltip: {
+      title: "Product Status",
+      message: "You can set the status of the product. If the product is closed, it will not be displayed in the online product list and product page.<br>e.g: <code>Open</code>",
+    },
   },
 
   Quantity: {
     type: "number",
     default: 0, // Autocorrection
+    tooltip: {
+      title: "Inventory Quantity",
+      message: "This is the quantity of the product. If the product is out of stock, you can set the quantity to zero.<br>e.g: <code>100</code>",
+    },
   },
 
   "Limit Min": {
     type: "number",
     default: 0,
     onlyProduct: true,
+    tooltip: {
+      title: "Minimum Order Quantity",
+      message: "You can set the minimum order quantity of the product. If the minimum order quantity is zero, there is no limit.<br>e.g: <code>10</code>",
+    },
   },
   "Limit Max": {
     type: "number",
     default: 0,
     onlyProduct: true,
+    tooltip: {
+      title: "Maximum Order Quantity",
+      message: "You can set the maximum order quantity of the product. If the maximum order quantity is zero, there is no limit.<br>e.g: <code>100</code>",
+    },
   },
 
   Condition: {
@@ -250,11 +333,19 @@ export const ProductsCsvStyler = {
     onlyProduct: true,
     required_product: true,
     default: ProductCondition.NEW.code, // Autocorrection
+    tooltip: {
+      title: "Product Condition",
+      message: "You can set the condition of the product. If the product is new, you can set the condition to new.<br>e.g: <code>NEW</code>",
+    },
   },
 
   Brand: {
     type: "text",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Brand",
+      message: "You can set the brand of the product. If the product is Apple, you can set the brand to Apple.<br>e.g: <code>Apple</code>",
+    },
   },
   Warranty: {
     type: "text",
@@ -280,6 +371,12 @@ export const ProductsCsvStyler = {
   Image: {
     type: "image",
     onlyProduct: true,
+    tooltip: {
+      title: "Main Product Image",
+      message:
+        "You can set the main image of the product. This image will be shown in the product list and product page. The structure of the image is as follows: <code>image-url</code>",
+      image: require("./assets/col-image.jpg"),
+    },
   },
   Outputs: {
     type: "json",
@@ -292,18 +389,34 @@ export const ProductsCsvStyler = {
   "Content Title": {
     type: "text",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Article Title",
+      message: "You can set the title of the product article. This title will be shown in the product page.",
+    },
   },
   "Content Body (Html)": {
     type: "html",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Article Body",
+      message: "You can set the body of the product article. It can be HTML code. This body will be shown in the product page.",
+    },
   },
   "Content Description": {
     type: "text",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Article Description (SEO)",
+      message: "It's the description of the product article. It should be short and clear.",
+    },
   },
   "Content Image": {
     type: "image",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Article Image (SEO)",
+      message: "It's the image url of the product article.",
+    },
   },
   "Content FAQ": {
     type: "faqs",
@@ -316,9 +429,17 @@ export const ProductsCsvStyler = {
   Category: {
     type: "text",
     onlyProduct: true,
+    tooltip: {
+      title: "Product Category",
+      message: "You can enter the name of category of the product. System will try to find the category and assign it to the product. If the category doesn't exist, it will be created.<br>e.g: <code>Electronics</code>",
+    },
   },
   "Lead Time": {
     type: "number",
+    tooltip: {
+      title: "Lead Time",
+      message: "It's the lead time of the product. It's the time between the order and the shipment of the product. If you don't know the lead time enter -1.<br>e.g: <code>3</code>",
+    },
   },
   Extra: {
     type: "extra",
@@ -327,10 +448,18 @@ export const ProductsCsvStyler = {
     type: "boolean",
     onlyProduct: true,
     default: false, // Autocorrection
+    tooltip: {
+      title: "Product Image Contain",
+      message: "Images can be contain or cover.",
+    },
   },
   "Return Warranty": {
     type: "number",
     onlyProduct: true,
+    tooltip: {
+      title: "Return Warranty",
+      message: "It's the hours that customer can return the product after delivery. <br>e.g for 1 day: <code>24</code>",
+    },
   },
   Original: {
     type: "boolean",
@@ -344,6 +473,12 @@ export const ProductsCsvStyler = {
   },
   Images: {
     type: "images",
+    tooltip: {
+      title: "Gallery Images",
+      message:
+        "You can add multiple images to the product gallery. The structure of the images is as follows: <code>image1-url.jpg</code>,<code>image2-url.jpg</code>,<code>image3-url.jpg</code>,...",
+      image: require("./assets/col-images.png"),
+    },
   },
   V_Color: {
     type: "color",
