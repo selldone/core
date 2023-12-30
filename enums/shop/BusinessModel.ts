85025e49250e0cf967310de26a992acd84f3ecc2
Pajuhaan <pajuhaan@gmail.com>
2023-12-30 18:50:38 +0400

    Update SDK Update limits from 100 to 200 items for app

--- enums/shop/BusinessModel.ts
+++ enums/shop/BusinessModel.ts
@@ -22,19 +22,15 @@ export const BusinessModel = {
     code: "wholesaler",
     name: "business_model_view.wholesaler.title",
     desc: "business_model_view.wholesaler.desc",
-
   },
   MARKETPLACE: {
     code: "marketplace",
     name: "business_model_view.marketplace.title",
     desc: "business_model_view.marketplace.desc",
-
   },
   FRANCHISE: {
     code: "franchise",
     name: "business_model_view.franchise.title",
     desc: "business_model_view.franchise.desc",
-
   },
-
 };
