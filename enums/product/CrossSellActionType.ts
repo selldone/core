043e3e8702c10f5803f5c505e5f352a1e9db9dff
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-08-13 20:50:58 +0400

    Update

--- enums/product/CrossSellActionType.ts
+++ enums/product/CrossSellActionType.ts
@@ -13,8 +13,16 @@
  */
 
 export default {
-    AddToCart:{code:'AddToCart',title:'Add to cart',description:"Display the Add to Cart button directly. Note that this is not applicable for subscription products.",icon:'local_mall'},
-    ViewProduct:{code:'ViewProduct',title:'View Product',description:"Display a View More button that directs customers to the product page by opening a new window.",icon:'open_in_new'},
-
-
-}
+  AddToCart: {
+    code: "AddToCart",
+    title: "CrossSellActionType.AddToCart.title",
+    description: "CrossSellActionType.AddToCart.description",
+    icon: "local_mall",
+  },
+  ViewProduct: {
+    code: "ViewProduct",
+    title: "CrossSellActionType.ViewProduct.title",
+    description: "CrossSellActionType.ViewProduct.description",
+    icon: "open_in_new",
+  },
+};
