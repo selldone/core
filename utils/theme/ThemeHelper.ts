812df829f608bd2b387e2161c81af15b892a24d2
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-08-26 16:30:19 +0400

    Update

--- utils/theme/ThemeHelper.ts
+++ utils/theme/ThemeHelper.ts
@@ -12,8 +12,46 @@
  * Tread carefully, for you're treading on dreams.
  */
 
+/**
+ * A helper class for managing theme-related functionalities, such as setting the theme color in the meta tag.
+ */
 export class ThemeHelper {
-  static SetMetaThemeColor(color:any) {
+  /**
+   * Sets the meta theme color in the document's head. If the meta tag does not exist, it creates one.
+   * This function can handle direct color values (e.g., hex, RGB, named colors) or CSS variables.
+   * If a CSS variable is provided (e.g., 'var(--background)'), the method will resolve the actual color value
+   * from the specified element in the DOM.
+   *
+   * @param color - The color to set for the theme, typically a string representing a hex code, RGB value, named color, or a CSS variable.
+   *                If a CSS variable is used, it should be in the format 'var(--variable-name)'.
+   * @param calculator_element - A CSS selector string that identifies the element from which the CSS variable's value should be computed.
+   *                             This defaults to '#app .v-application'. If your CSS variable is defined in a different element,
+   *                             provide the appropriate selector.
+   */
+  static SetMetaThemeColor(
+    color: any,
+    calculator_element: string = "#app .v-application",
+  ) {
+    if (!color) return;
+
+    try {
+      if (color.includes("var")) {
+        // Extract the variable name from the 'var(--background)' syntax
+        const variableName = color.match(/var\((--[^)]+)\)/)[1];
+
+        // Get the nested element within #app where the CSS variable is defined
+        const element = document.querySelector(calculator_element); // Replace '.nested-element' with your actual selector
+
+        // Get the computed styles for the element
+        const computedStyle = getComputedStyle(element);
+
+        // Retrieve the value of the CSS variable
+        color = computedStyle.getPropertyValue(variableName).trim();
+      }
+    } catch (e) {
+      console.error("SetMetaThemeColor error:", e);
+    }
+
     const metaThemeColor = document.querySelector('meta[name="theme-color"]');
     if (metaThemeColor) {
       metaThemeColor.setAttribute("content", color);
